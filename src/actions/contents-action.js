99ff8eeb3eda8eb15938be2f71b1575e5b22445f
2fps <echoweb@126.com>
2018-09-01 19:48:51 +0800

    移除原有代码

--- src/actions/contents-action.js
+++
@@ -1,8 +0,0 @@
-const PAGE_CONTETNS = 'PAGE_CONTETNS';
-
-export function pageContent (title, time, frequency, praise, acronym) {
-    return {
-        type: PAGE_CONTETNS,
-        newCon: {title, time, frequency, praise, acronym}
-    }
-};
